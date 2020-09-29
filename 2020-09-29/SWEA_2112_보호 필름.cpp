95f29d56f66aaba9845a96e4d6aafe97ff0a7e34
cheerful1313 <56524328+cheerful1313@users.noreply.github.com>
2020-09-29 21:30:52 +0900

    2020-09-29

---
+++ 2020-09-29/SWEA_2112_보호 필름.cpp
@@ -0,0 +1,141 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int T, D, W, K;
+int cell[13][20];
+
+vector<int> pick;
+
+int rslt = 0;
+
+bool check()
+{
+    int cntA, cntB;
+    bool flag;
+
+    for (int c = 0; c < W; c++) 
+    {
+        cntA = 0;
+        cntB = 0;
+        flag = false;
+
+        for (int r = 0; r < D; r++) //열 조사
+        {
+            if (cell[r][c] == 0) //A이면
+            {
+                cntA++; //A개수 증가
+                cntB = 0; //B개수 0으로 초기화
+            }
+            else
+            {
+                cntA = 0;
+                cntB++;
+            }
+
+            if (cntA == K || cntB == K) //A, B 둘 중 하나라도 K개 이상이면 통과
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag) //한 열이라도 통과하지 못했다면
+            return false;
+    }
+
+    return true;
+}
+
+bool injection(int now, int r) //약물 투입(현재 투입한 행, 행의 개수)
+{
+    int line[20];
+
+    if (now >= r) //투입 끝
+    {
+        if (check()) //검사했는데 합격
+            return true;
+        else
+            return false;
+    }
+
+    for (int kind = 0; kind <= 1; kind++) //A약물 = 0, B약물 = 1
+    {
+        for (int i = 0; i < W; i++) 
+        {
+            line[i] = cell[pick[now]][i]; //내가 고른 행(막)을 저장
+            cell[pick[now]][i] = kind; //약을 투입해서 바꿔줌(0 = A / 1 = B)
+        }
+
+        if (injection(now + 1, r)) //다음 행(막) 바꿔주기
+            return true;
+
+        for (int i = 0; i < W; i++)
+            cell[pick[now]][i] = line[i]; //다시 원래대로
+    }
+
+    return false;
+}
+
+bool comb(int cnt, int r) //D C r : D개의 행(막)에서 r개의 행(막)을 고른다
+{
+    if (pick.size() == r) //어떤 행(막)에 투입할지 다 골랐다
+    {
+        if (injection(0, r)) // 투입
+            return true;
+        else
+            return false;
+    }
+
+    for (int i = cnt; i < D; i++)
+    {
+        pick.push_back(i);
+        if (comb(i + 1, r))
+            return true;
+        pick.pop_back();
+    }
+
+    return false;
+}
+
+void solve()
+{
+    if (K == 1 || check()) //합격기준 1 or 주어진 그대로 합격
+        rslt = 0;
+    else //약품 투입
+    {
+        rslt = K; 
+        for (int i = 1; i < K; i++) //적어도 K번보다는 적게 투여해야 함
+        {
+            pick.clear();
+            if (comb(0, i)) //만약 i회 투입에서 성공
+            {
+                rslt = i; //최소 투입 횟수 갱신
+                break;
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin >> T;
+    for (int tc = 1; tc <= T; tc++)
+    {
+        //init
+        rslt = 0;
+
+        //input
+        cin >> D >> W >> K;
+        for (int i = 0; i < D; i++)
+        {
+            for (int j = 0; j < W; j++)
+                cin >> cell[i][j];
+        }
+
+        solve();
+
+        //output
+        cout << "#" << tc << " " << rslt << endl;
+    }
+}
---
+++ 2020-09-29/SWEA_4613_러시아 국기 같은 깃발.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+//#define DEBUG
+#define MAX 50
+
+int T, N, M;
+char cell[MAX][MAX];
+int color[MAX][3]; //각 줄마다의 색 개수 (0 = W, 1 = B, 2 = R)
+int rslt, base; //결과값, 기본값
+
+void range_of_blue(int sr, int er) //파랑 범위 기준으로 색 바꾸기
+{
+    int ans = base;
+
+    //white : 파랑 기준으로 위
+    for (int i = 1; i < sr; i++)
+        ans += (color[i][1] + color[i][2]); 
+
+    //blue
+    for (int i = sr; i < er; i++)
+        ans += (color[i][0] + color[i][2]); 
+
+    //red : 파랑 기준으로 아래
+    for (int i = er; i < N - 1; i++)
+        ans += (color[i][0] + color[i][1]); 
+
+    rslt = rslt < ans ? rslt : ans; //최소값 찾기
+}
+
+void solve()
+{
+    //init
+    memset(color, 0, sizeof(color));
+    base = 0; //맨 위, 맨 아래 변경 값은 고정
+    rslt = MAX * MAX;
+
+    for (int c = 0; c < M; c++)
+    {
+        if (cell[0][c] != 'W') //맨 윗줄은 항상 흰색
+            base++;
+        if (cell[N - 1][c] != 'R') //맨 아랫줄은 항상 빨간색
+            base++;
+    }
+
+    for (int r = 1; r < N - 1; r++) //각 줄마다 색 개수 세기
+    {
+        for (int c = 0; c < M; c++)
+        {
+            if (cell[r][c] == 'W')
+                color[r][0]++;
+            else if (cell[r][c] == 'B')
+                color[r][1]++;
+            else //R
+                color[r][2]++;
+        }
+    }
+
+    //파랑의 범위를 정하기
+    for (int line = 1; line <= N-2; line++) //파랑은 최소 1줄부터 최대 N-2줄까지
+    {
+        for (int r = 1; r + line < N; r++)
+            range_of_blue(r, r + line);
+    }
+}
+
+int main() 
+{  
+    cin >> T;
+    for (int tc = 1; tc <= T; tc++)
+    {
+        //input
+        cin >> N >> M;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+                cin >> cell[i][j];
+        }
+
+        solve();
+
+        //output
+        cout << "#" << tc << " " << rslt << endl;
+    }
+}
