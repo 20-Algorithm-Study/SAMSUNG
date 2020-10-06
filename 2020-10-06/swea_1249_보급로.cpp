f681afeb05144fa97d7e3455845ce5f60cd89778
cheerful1313 <56524328+cheerful1313@users.noreply.github.com>
2020-10-06 20:55:36 +0900

    Add files via upload

---
+++ 2020-10-06/swea_1249_보급로.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+#define MAX 100
+
+int T, N;
+char map[MAX][MAX];
+
+struct Loc {
+    int r, c;
+    Loc(int _r, int _c) { r = _r; c = _c; }
+};
+queue <Loc> q;
+int visited[MAX][MAX] = { 0, };
+int dir[4][2] = { {-1,0}, {1,0}, {0,1}, {0,-1} };
+
+void solve()
+{
+    int r, c, nr, nc;
+
+    //init
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+            visited[i][j] = 90000;
+    }
+
+    q.push(Loc(0, 0));
+    visited[0][0] = map[0][0] - '0';
+
+    while (!q.empty())
+    {
+        r = q.front().r;
+        c = q.front().c;
+        q.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            nr = r + dir[i][0];
+            nc = c + dir[i][1];
+
+            if (nr < 0 || nc < 0 || nr >= N || nc >= N) //범위 넘어갔음
+                continue;
+
+            if (visited[r][c] + map[nr][nc] - '0' >= visited[nr][nc]) //새로운 경로가 더 복구시간이 길면 갈 필요 없음
+                continue;
+
+            visited[nr][nc] = visited[r][c] + map[nr][nc] - '0';
+
+            if (nr == N - 1 && nc == N - 1) //도착지 왔으면 갱신만 하고 큐에 넣을 필요는 없음
+                continue;
+            else
+                q.push(Loc(nr, nc));
+        }
+    }
+}
+
+int main()
+{
+    cin >> T;
+    for (int tc = 1; tc <= T; tc++)
+    {
+        //input
+        cin >> N;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+                cin >> map[i][j];
+        }
+
+        solve();
+
+        //output
+        cout << "#" << tc << " " << visited[N - 1][N - 1] << endl;
+    }
+}
---
+++ 2020-10-06/swea_1953_탈주범 검거.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <queue>
+#include <cstring>
+using namespace std;
+#define MAX 50
+
+int T, N, M, R, C, L;
+int map[MAX][MAX];
+int answer = 0;
+
+struct Loc {
+    int r, c;
+    Loc(int _r, int _c) { r = _r; c = _c; }
+};
+queue <Loc> q;
+int visited[MAX][MAX] = { 0, };
+int dir[8][4][2] = {{{0,0}, {0,0}, {0,0}, {0,0}  }, //0
+                    {{-1,0}, {1,0}, {0,-1}, {0,1}}, //1. 상 하 좌 우
+                    {{-1,0}, {1,0}, {0,0}, {0,0} }, //2. 상 하 
+                    {{0,0}, {0,0}, {0,-1}, {0,1} }, //3.       좌 우
+                    {{-1,0}, {0,0}, {0,0}, {0,1} }, //4. 상       우
+                    {{0,0}, {1,0}, {0,0}, {0,1}  }, //5.    하    우
+                    {{0,0}, {1,0}, {0,-1}, {0,0} }, //6.    하 좌
+                    {{-1,0}, {0,0}, {0,-1}, {0,0}}, //7. 상    좌 
+                    }; 
+
+void solve()
+{
+    int r, c, d, nr, nc;
+    
+    answer = 0;
+    memset(visited, 0, sizeof(visited));
+
+    q.push(Loc(R, C));
+    visited[R][C] = 1;
+
+    while (!q.empty())
+    {
+        r = q.front().r;
+        c = q.front().c;
+        d = map[r][c];
+        answer++;
+        q.pop();
+
+        if (visited[r][c] >= L) //탈출 후 소요된 시간
+            continue;
+
+        for (int i = 0; i < 4; i++)
+        {
+            nr = r + dir[d][i][0];
+            nc = c + dir[d][i][1];
+
+            if (nr < 0 || nc < 0 || nr >= N || nc >= M || visited[nr][nc] > 0) //범위 넘어감 or 이미 방문
+                continue;
+
+            if (map[nr][nc] == 0)
+                continue;
+            
+            if (i == 0 && map[nr][nc] != 1 && map[nr][nc] != 2 && map[nr][nc] != 5 && map[nr][nc] != 6) //상
+                continue;
+            else if (i == 1 && map[nr][nc] != 1 && map[nr][nc] != 2 && map[nr][nc] != 4 && map[nr][nc] != 7) //하
+                continue;
+            else if (i == 2 && map[nr][nc] != 1 && map[nr][nc] != 3 && map[nr][nc] != 4 && map[nr][nc] != 5) //좌
+                continue;
+            else if (i == 3 && map[nr][nc] != 1 && map[nr][nc] != 3 && map[nr][nc] != 6 && map[nr][nc] != 7) //우
+                continue;
+
+            cout << r << " " << c << " : " << nr << " " << nc << endl;
+            q.push(Loc(nr, nc));
+            visited[nr][nc] = visited[r][c] + 1;
+        }
+    }
+}
+
+int main()
+{
+    cin >> T;
+    for (int tc = 1; tc <= T; tc++)
+    {
+        //input
+        cin >> N >> M >> R >> C >> L;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+                cin >> map[i][j];
+        }
+
+        solve();
+
+        //output
+        cout << "#" << tc << " " << answer << endl;
+    }
+}
